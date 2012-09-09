fd48a7bc52ec82b5437c91355911f068ff209367
Augusto Pascutti <augusto@phpsp.org.br>
2012-09-09 19:50:11 -0300

    Refactor of application boostrap and configuration.
    - Bootstrap now support two basic environments:
    	- develpment: All errors are logged and shown on screen.
        - production: Only severe errors are logged. (Default)
    - Configurations moved to environment variables constants:
        - Database configuration
        - Foursquare OAuth2 tokens
    - Moves twig_render() from index.php to bootstrap.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,7 @@
 <?php
-require 'bootstrap.php';
-
 try {
+    require 'bootstrap.php';
     $config = new Respect\Config\Container('conf/app.ini');
-    function twig_render($vars) {
-        global $config;
-        $twig = $config->container->twig;
-        return call_user_func_array(array($twig, 'render'), $vars);
-    };
     echo $config->container->router->run();
 } catch (Exception $e) {
     var_dump($e);
